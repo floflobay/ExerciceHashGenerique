2a37472374bde629c4e11ee9bf17e85e01b1a0df
floflobay <fbayeux@gmail.com>
2018-01-11 12:33:15 +0000

    essai 1.4
    Ajout d'un seul DLL Hash en fonction du References choisi
    (Ajouter soir SHA3 soit SHA256 soit NAIVEHASH )
    La verison marche toujours
    Suppression de . DLL au prochain commit

--- ExerciceHashGenerique/main.cpp
+++ ExerciceHashGenerique/main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
 	for (auto it = test_vector.begin(); it<test_vector.end(); it++){
 		output=hash(*it);
 
-		cout <<  "hashName_V" << version() << "()" << "('"<< *it << "'):" << output << endl;
+		cout << "Name of hash " << hashName() << "hashName_V" << version() << "()" << "('"<< *it << "'):" << output << endl;
 
 	}
 }
--- sha256/sha256.h
+++ sha256/sha256.h
@@ -2,7 +2,6 @@
 #ifndef SHA256_H
 #define SHA256_H
 #include <string>
-
 #define SHA256_VERSION 1,0,0,1
 #define SHA256_VERSION_STR "1,0,0,1"
 
